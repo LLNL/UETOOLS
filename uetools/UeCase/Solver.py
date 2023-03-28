b8663da2c3516707003c41a2a39e25bf3b3fd99e
Andreas Holm <60451789+holm10@users.noreply.github.com>
2023-03-27 18:19:52 -0700

    Added auspices

--- uetools/UeCase/Solver.py
+++ uetools/UeCase/Solver.py
@@ -1,9 +1,9 @@
 # Package setting up solvers, time-stepping, etc
 # Holm10 Dec 10 2022, based on rundt.py
-from uedge.rundt import RunData
+from uedge.rundt import UeRun
 from Forthon import packageobject
 
-class Solver(RunData):
+class Solver(UeRun):
     def __init__():
         super(Solver, self).__init__()
         return
