da0871738dd6942ddeb2ca7fa4ec5902667cf07b
Andreas Holm <60451789+holm10@users.noreply.github.com>
2023-08-17 15:38:55 -0700

    Initial commit of ParallelGridPatch

--- uetools/UePostproc/Postproc.py
+++ uetools/UePostproc/Postproc.py
@@ -3,11 +3,21 @@ class PostProcessors:
     def postproc_helloworld(self):
         print('Hello world!')
 
-    def set_psinormc(self, simagxs, sibdrys):
+    def set_psinormc(self, simagxs=None, sibdrys=None):
+        if simagxs is None:
+            simagxs = self.get('simagxs')
+        if sibdrys is None:
+            sibdrys = self.get('sibdrys')
+
         self.psinormc =  (self.get('psi')[self.get('ixmp'),:,0]-simagxs)/ \
             (sibdrys-simagxs)
 
-    def set_psinormf(self, simagxs, sibdrys):
+    def set_psinormf(self, simagxs=None, sibdrys=None):
+        if simagxs is None:
+            simagxs = self.get('simagxs')
+        if sibdrys is None:
+            sibdrys = self.get('sibdrys')
+
         psi = self.get('psi')[self.get('ixmp')]
         self.psinormf = ((0.5*(psi[:,3] + psi[:,4])) -simagxs)/ \
             (sibdrys-simagxs)
--- uetools/UeUtils/Interpolate.py
+++ uetools/UeUtils/Interpolate.py
@@ -13,13 +13,20 @@ class Interpolate():
         """ Interpolates new solution based on previous state and new grid """
         from h5py import File
 
+#       TODO: Make oldgrid option. If not supplied, use variables in memory.
+#       TODO: Read and pass magnetic and physical data: calculate poloidal/
+#           parallel distance between end-points of each flux-tube.
+#           Start by using flux-tube averaged values? Will let one use 
+#           RegularGridInterpolator. Then, try to use unstructured grid
+#           interpolators instead.
+
         if ishdf5 is None:
             ishdf5 = self.get('isgriduehdf5')
         # Start by reading in new and old grid dimensions
         grid_old = {}
         grid_new = {}
         # Check if gridue is HDF5 file or not: read accordingly
-        proplist = ['nxm', 'nym', 'ixpt1', 'ixpt2', 'iysptrx1']
+        proplist = ['nxm', 'nym', 'ixpt1', 'ixpt2', 'iysptrx1', 'b', 'bpol', 'rm', 'zm']
         if ishdf5==True:
             with File(oldgrid) as f_oldgrid:
                 for prop in proplist:
@@ -80,8 +87,8 @@ class Interpolate():
                 'grid dimensions. Grid (nx, ny) = ({}, {}), save (nx, ny) = '
                 '({}, {})'.format(nx_old, ny_old, save_nx-2, save_ny-2))
     
-        self.oldgrid = GridSnull(grid_old, savedata, radtransp)
-        newgrid = self.oldgrid.interpolate_grid(grid_new)
+        self.oldgrid = GridSnull(grid_old, savedata, radtransp, **kwargs)
+        newgrid = self.oldgrid.interpolate_grid(grid_new, **kwargs)
         newsave = newgrid.savedata
         newradtransp = newgrid.radtransp
         if newsavename is None:
@@ -113,12 +120,12 @@ class Interpolate():
 
 class GridSnull:
     """ Object containing single null grid data for interpolation """
-    def __init__(self, dimensions, savedata, radtransp):
-        self.nx = dimensions['nxm']
-        self.ny = dimensions['nym']
-        self.ixpt1 = dimensions['ixpt1']
-        self.ixpt2 = dimensions['ixpt2']
-        self.iysptrx = dimensions['iysptrx1']
+    def __init__(self, griddata, savedata, radtransp, interpolation='index'):
+        self.nx = griddata['nxm']
+        self.ny = griddata['nym']
+        self.ixpt1 = griddata['ixpt1']
+        self.ixpt2 = griddata['ixpt2']
+        self.iysptrx = griddata['iysptrx1']
         self.savedata = savedata
         self.radtransp = radtransp
         # Define the snull topology:
@@ -130,48 +137,61 @@ class GridSnull:
         # CORE |            |                   |           |
         #      |____________|___________________|___________|
         #           ILEG             CORE            OLEG
+
+        if interpolation.lower() == 'index':
+            PatchType = IndexGridPatch
+            self.connlen = None
+        elif interpolation.lower() == 'parallel':
+            PatchType = ParallelGridPatch
+            self.connlen = self.calc_connlen(griddata)
+
+
+        else:
+            raise ValueError('Interpolation type "{interpolation}" not recognized!')
+            
         self.patches = {
             'sol': {
-#                'common': IndexGridPatch(0, self.nx+2, self.iysptrx+1, 
-#                            self.ny+2, savedata, radtransp)
-                'ileg': IndexGridPatch(0, self.ixpt1+1, self.iysptrx, 
-                            self.ny+2, savedata, radtransp),
+                'ileg': PatchType(0, self.ixpt1+1, self.iysptrx, 
+                            self.ny+2, savedata, radtransp, self.connlen),
                 'core': IndexGridPatch(self.ixpt1+1, self.ixpt2+1, self.iysptrx, 
                             self.ny+2, savedata, radtransp),
-                'oleg': IndexGridPatch(self.ixpt2+1, self.nx+2, self.iysptrx, 
-                            self.ny+2, savedata, radtransp),
+                'oleg': PatchType(self.ixpt2+1, self.nx+2, self.iysptrx, 
+                            self.ny+2, savedata, radtransp, self.connlen),
             },
             'core': {
-                'ileg': IndexGridPatch(0, self.ixpt1+1, 0, 
-                            self.iysptrx+1, savedata, radtransp),
+                'ileg': PatchType(0, self.ixpt1+1, 0, 
+                            self.iysptrx+1, savedata, radtransp, self.connlen),
                 'core': IndexGridPatch(self.ixpt1+1, self.ixpt2+1, 0, 
                             self.iysptrx+1, savedata, radtransp),
-                'oleg': IndexGridPatch(self.ixpt2+1, self.nx+2, 0, 
-                            self.iysptrx+1, savedata, radtransp),
+                'oleg': PatchType(self.ixpt2+1, self.nx+2, 0, 
+                            self.iysptrx+1, savedata, radtransp, self.connlen),
             }
         }
 
-    def interpolate_grid(self, dimensions):
+    def interpolate_grid(self, griddata, **kwargs):
         """ Interpolates the current grid to a new GridSnull object """ 
         from numpy import concatenate
-        nx_new = dimensions['nxm']
-        ny_new = dimensions['nym']
-        ixpt1_new = dimensions['ixpt1']
-        ixpt2_new = dimensions['ixpt2']
-        iysptrx_new = dimensions['iysptrx1']
+        nx_new = griddata['nxm']
+        ny_new = griddata['nym']
+        ixpt1_new = griddata['ixpt1']
+        ixpt2_new = griddata['ixpt2']
+        iysptrx_new = griddata['iysptrx1']
+        connlen_new = self.calc_connlen(griddata)
+
+        
         
         # Store the new dimensions
         nxny = {
             'sol':{
 #                'common': [nx_new+2, ny_new-iysptrx_new+1]
-                'ileg': [ixpt1_new+1, ny_new+2-(iysptrx_new+1)],
-                'core': [ixpt2_new-ixpt1_new, ny_new+2-(iysptrx_new+1)],
-                'oleg': [nx_new-ixpt2_new+1 , ny_new+2-(iysptrx_new+1)]
+                'ileg': [0, ixpt1_new+1, iysptrx_new+1, ny_new+2, connlen_new],
+                'core': [ixpt1_new+1, ixpt2_new+1, iysptrx_new+1, ny_new+2],
+                'oleg': [ixpt2_new+1, nx_new+2, iysptrx_new+1, ny_new+2, connlen_new]
             },
             'core':{
-                'ileg': [ixpt1_new+1, iysptrx_new+1],
-                'core': [ixpt2_new-ixpt1_new, iysptrx_new+1],
-                'oleg': [nx_new-ixpt2_new+1 , iysptrx_new+1]
+                'ileg': [0, ixpt1_new+1, 0, iysptrx_new+1, connlen_new],
+                'core': [ixpt1_new+1, ixpt2_new+1, 0, iysptrx_new+1],
+                'oleg': [ixpt2_new+1, nx_new+2, 0, iysptrx_new+1, connlen_new]
             }
         }
 
@@ -185,7 +205,6 @@ class GridSnull:
                 try:
                     patches_new[radialkey][poloidalkey]
                 except:
-                    print(poloidalkey, radialkey, nxny[radialkey][poloidalkey])
                     patches_new[radialkey][poloidalkey] = \
                         patch.interpolate_solution(*nxny[\
                         radialkey][poloidalkey])
@@ -204,7 +223,6 @@ class GridSnull:
                         patches_new['sol']['core'][variable],
                         patches_new['sol']['oleg'][variable]
                     ))
-#                    patches_new['sol']['common'][variable]
                 ), axis = 1)
          
         radtransp_new = {}
@@ -223,7 +241,6 @@ class GridSnull:
                                 patches_new['sol']['core'][variable],
                                 patches_new['sol']['oleg'][variable]
                             ))
-#                            patches_new['sol']['common'][variable]
                         ), axis = 1)   
                 except:
                     radtransp_new[vartype][variable] = \
@@ -232,13 +249,47 @@ class GridSnull:
         # TODO:
         # Add interpolation in 1D
                     
-        return GridSnull(dimensions, savedata_new, radtransp_new)
+        return GridSnull(griddata, savedata_new, radtransp_new, **kwargs)
+
+    def calc_connlen(self, griddata):
+        ''' Calculates the parallel connection length '''
+        from copy import deepcopy
+        from numpy import zeros
+        # Calculate the parallel length of each cell
+        # Pad with negligible value to avoid zero-divisor
+        rbfbt = (deepcopy(griddata['b'])/(deepcopy(griddata['bpol'])+1e-50))[:,:,0]
+        # And again for future use
+        rbfbt[rbfbt==0] = 1
+        rm = griddata['rm']
+        zm = griddata['zm']
+        (nxg, nyg, _) = rm.shape
+        midpoints = zeros((2, nxg+1, nyg))
+        # (R,Z)-coordinates of the midpoints
+        midpoints[0,:-1] = 0.5*(rm[:,:,1]+rm[:,:,3])
+        midpoints[1,:-1] = 0.5*(zm[:,:,1]+zm[:,:,3])
+        # Do right boundary
+        midpoints[0,-1] = 0.5*(rm[-1,:,2]+rm[-1,:,4])
+        midpoints[1,-1] = 0.5*(zm[-1,:,2]+zm[-1,:,4])
+        # Calculate poloidal distance for each cell
+        polloc = midpoints[:,1:] - midpoints[:,:-1]
+        pollength = (polloc[0]**2 + polloc[1]**2)**0.5
+        # Caclulate the parallel length of each cell
+        parlength = pollength * rbfbt
+        # Calculate parallel distance from left boundary
+        connlen = zeros((nxg, nyg))
+        for i in range(1,nxg):
+            connlen[i] += connlen[i-1] + parlength[i]
+        # Set guard cells
+        connlen[:,0] = connlen[:,1]
+        connlen[:,-1] = connlen[:,-2]
+        return connlen
+
             
         
 
 class IndexGridPatch:
     """ Object containing data for topological patch """
-    def __init__(self, nxl, nxu, nyl, nyu, savedata, radtransp):
+    def __init__(self, nxl, nxu, nyl, nyu, savedata, radtransp, *args, **kwargs):
         """ Set up the required interpolators """
         from numpy import linspace
         from scipy.interpolate import RegularGridInterpolator
@@ -284,10 +335,12 @@ class IndexGridPatch:
 
            
 
-    def interpolate_solution(self, nx, ny):
+    def interpolate_solution(self, nxl, nxu, nyl, nyu, *args):
         """ Returns an interpolation of solution in index space """
         from numpy import linspace, meshgrid
         new_solution = {}
+        nx = nxu - nxl
+        ny = nyu - nyl
         x = linspace(0, 1, nx)
         y = linspace(0, 1, ny)
         X, Y = meshgrid(x, y, indexing='ij')
@@ -302,4 +355,122 @@ class IndexGridPatch:
                 new_solution[variable] = interpolator[0]((Xz, Yz, Z))
         return new_solution
         
+
+class ParallelGridPatch:
+    """ Object containing data for topological patch """
+    def __init__(self, nxl, nxu, nyl, nyu, savedata, radtransp, connlen):
+        """ Set up the required interpolators """
+        from numpy import linspace, zeros
+        from scipy.interpolate import griddata
+        from copy import deepcopy
+        self.nxl = nxl
+        self.nxu = nxu
+        self.nx = nxu - nxl # Number of nodes in X-direction of patch
+        self.nyl = nyl
+        self.nyu = nyu
+        self.ny = nyu - nyl # Number of nodes in Y-direction of patch
+        self.savedata = deepcopy(savedata)
+        self.radtransp = deepcopy(radtransp)
+        self.connlen = deepcopy(connlen)
+
+        self.xy = self.get_xy(nxl, nxu, nyl, nyu, 
+            self.nx, self.ny, self.connlen)
+        # Break data into patch and create interpolation functions
+        self.interp = {}
+        for variable, data in self.savedata.items():
+            self.savedata[variable] = data[nxl:nxu, nyl:nyu]
+            self.interp[variable] = []
+            if len(data.shape) == 3:
+                nz = data.shape[2]
+                for iz in range(nz):
+                # Append list [ (x,y), z ] containing data necessary for
+                # interpolation 
+                    self.interp[variable].append([
+                            self.xy, self.savedata[variable][:,:,iz].ravel()
+                        ]
+                    )
+            else:
+                # Do interpolation with single species
+                self.interp[variable].append([
+                        self.xy, self.savedata[variable].ravel()
+                    ]
+                )
+
+        for variable, data in self.radtransp['2D'].items():
+            self.radtransp[variable] = data[nxl:nxu, nyl:nyu]
+            self.interp[variable] = []
+            if len(data.shape) == 3:
+                nz = data.shape[2]
+                # Do interpolation with multiple species
+                for iz in range(nz):
+                    self.interp[variable].append([
+                            self.xy, self.radtransp[variable][:,:,iz].ravel()
+                        ]
+                    )
+            else:
+                # Do interpolation with single species
+                self.interp[variable].append([
+                        self.xy, self.radtransp[variable].ravel()
+                    ]
+                )
+           
+
+    def interpolate_solution(self, nxl, nxu, nyl, nyu, connlen):
+        """ Returns an interpolation of solution in index space """
+        from numpy import linspace, meshgrid, array, mean
+        from scipy.interpolate import griddata
+        from copy import deepcopy
+        new_solution = {}
+        nx = nxu - nxl
+        ny = nyu - nyl
+        print('INTERPOLATING')
+        connlen = deepcopy(connlen)
+        newpoints = self.get_xy(nxl, nxu, nyl, nyu, nx, ny, connlen)
+        for variable, interpolator in self.interp.items():
+            new_solution[variable] = []
+            for i in range(len(interpolator)):
+                new_solution[variable].append(
+                    griddata(
+                        interpolator[i][0], interpolator[i][1], newpoints,
+#                        fill_value = -1
+                    ).reshape((nx, ny))
+                )
+#                meanval = mean(new_solution[variable][-1][1:-1,1:-1])
+#                new_solution[variable][-1][new_solution[variable][-1]==-1] = meanval
+        for variable, solution in new_solution.items():
+            if len(solution) == 1:
+                new_solution[variable] = new_solution[variable][0]
+            else:
+                print(nx, ny, array(new_solution[variable]).shape)
+                new_solution[variable] = array(new_solution[variable]).\
+                    transpose((1,2,0))
+            # Pop into arrays according to shape
+        return new_solution
+        
+
+    def get_xy(self, nxl, nxu, nyl, nyu, nx, ny, connlen):
+        from numpy import linspace, zeros, concatenate
+        from copy import deepcopy
+        # Truncate connection length to correspond to patch
+        x = connlen[nxl:nxu, nyl:nyu]
+        # Check whether we are starting from the right
+        # TODO: other indicators needed for DNULLS!
+        xzero = deepcopy(x[0])
+        for i in range(x.shape[0]):
+            x[i] -= xzero # Make sure left bound starts at zero
+            x[i] /= x[-1] # Make sure right boundary ends at 1
+#        if nxl == 0:
+#            # Normalize to PFR cut, i.e. right side
+#            xnorm = deepcopy(x[-1])
+#        elif nxu == connlen.shape[0]:
+#            # Normalize to PFR cut, i.e. left side
+#            xnorm = deepcopy(x[0])
+#        else:
+#            raise ValueError('Patch could not be assinged topological L/R location!')
+#        for i in range(x.shape[0]):
+#            x[i] -= xnorm
+        y = zeros((nx, ny))
+        for i in range(y.shape[0]):
+            y[i] = linspace(0, 1, ny) 
+        return x.ravel(), y.ravel()
 
