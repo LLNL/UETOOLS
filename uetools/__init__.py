e3a305c29618677ffac2896984080620f313a758
Andreas Holm <60451789+holm10@users.noreply.github.com>
2023-08-21 14:37:12 -0700

    Fix for standalone import and use of UETOOLS

--- uetools/__init__.py
+++ uetools/__init__.py
@@ -1,4 +1,9 @@
-from uedge import bbb, com, grd, flx, aph, api
+# Try to import UEDGE packages into the namespace
+try:
+    from uedge import bbb, com, grd, flx, aph, api
+# In case no UEDGE is installed, only standalone mode is allowed
+except:
+    print('No UEDGE install found. Importing UEDGE Toolbox in standalone mode')
 from .UeCase import Case
 from .UeDatabase import Database, restoredb
 from . import UeCase
