b3e5c6c6a432779b1c869a9afe4431e7a9efc8d3
Andreas Holm <60451789+holm10@users.noreply.github.com>
2024-07-01 19:32:04 -0700

    Adds Misc utilitites
    - squareinterp: interpolates data on UEDGE grids onto a uniformly spaced (x,y)-grid
    - plot_gridue: plots the gridue data from a HDF5 gridue file

--- src/uetools/UeUtils/Misc.py
+++ src/uetools/UeUtils/Misc.py
@@ -5,6 +5,7 @@ class Misc():
         """ Stores connection length in Case.lcon """
         from numpy import cumsum
         try:
+            #TODO: How to extend to dnulls????
             # Calc and store connection lengths
             self.lcon = cumsum(1/(self.get('rr')*self.get('gx')+1e-20), axis=0)
             # No connlen for core cells
@@ -47,6 +48,7 @@ class Misc():
         return keys
 
     def potent_1dsol(self):
+        #TODO: How to extend to dnulls????
         from numpy import zeros_like
         phi = self.get('phi')
         phis = zeros_like(phi)
@@ -259,3 +261,73 @@ class Misc():
             res = cases
 
         return len(res)
+
+    def squareinterp(self, data, r=None, z=None, method='linear', 
+        resolution=(500j, 800j), mask=False, fill=float('NaN')):
+        from scipy.interpolate import griddata
+        from numpy import floor, ceil, mgrid, array, nan, concatenate
+        from shapely.geometry import Point
+        from shapely.geometry.polygon import Polygon
+        # Get R,Z coords
+        rm = self.get("rm")
+        zm = self.get("zm")
+        if self.get("geometry")[0].strip().lower().decode("UTF-8") == "     uppersn":        
+            zm = self.disp - zm
+        if mask:
+            # Mask out external points
+            x = []
+            y = []
+            for j in range(rm.shape[1]):
+                x.append(rm[1, j, 1])
+                y.append(zm[1, j, 1])
+            for i in range(rm.shape[0]):
+                x.append(rm[i, -1, 1])
+                y.append(zm[i, -1, 1])
+            for j in range(rm.shape[1]-1, -1, -1):
+                x.append(rm[-1, j, 1])
+                y.append(zm[-1, j, 1])
+            for i in range(rm.shape[0]-1, self.ixpt2, -1):
+                x.append(rm[i, 0, 4])
+                y.append(zm[i, 0, 4])
+            for i in range(self.ixpt1, -1, -1):
+                x.append(rm[i, 0, 4])
+                y.append(zm[i, 0, 4])
+            if self.get("geometry")[0].strip().lower().decode("UTF-8") == "     uppersn":        
+                y = [self.disp-a for a in y]
+            outline = Polygon(concatenate([[array(x), array(y)]], axis=0).T)
+            
+
+        # Automatically set sqare grid boundaries
+        if r is None:
+            r = (0.01*floor(rm.min()*100), 0.01*ceil(rm.max()*100))
+        if z is None:
+            z = (0.01*floor(zm.min()*100), 0.01*ceil(zm.max()*100))
+        # Get cell centers and unravel data
+        gx, gy = mgrid[r[0]:r[1]:resolution[0], z[0]:z[1]:resolution[1]]
+                
+        interp = griddata( (rm[:,:,0].ravel(), zm[:,:,0].ravel()), data.ravel(), (gx, gy), method=method, fill_value=fill)
+        if mask:
+            for i in range(gx.shape[0]):
+                for j in range(gx.shape[1]):
+                    if not outline.contains(Point(gx[i,j], gy[i,j])):
+                        interp[i,j] = nan
+
+        # Perform interpolation
+        return gx, gy, interp
+
+
+    def plot_gridue(self, fname):
+        from h5py import File
+        with File(fname) as f:
+            rm = f['grid/com/rm'][()]
+            zm = f['grid/com/zm'][()]
+        self.grid(rm=rm, zm=zm)
+    
+
+
+#     from uetools.UePostproc.ADASclass import ADASSpecies
+#     self.rates = ADASSpecies(path, species, ratetype, **kwargs)
+
+
+    
+    
