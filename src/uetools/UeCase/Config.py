1cddfc302502cabd6701a522fdf3a50baf946195
Andreas Holm <60451789+holm10@users.noreply.github.com>
2024-07-30 15:11:37 -0700

    Updates Config documentation and applies Black

--- src/uetools/UeCase/Config.py
+++ src/uetools/UeCase/Config.py
@@ -1,14 +1,40 @@
 # Reads/creates a config environment for UEDGE
 from uetools.UeUtils.Lookup import Lookup
 
-class Config():
+
+class Config:
+    """Class for creating and reading .uedgerc config file
+
+    Methods
+    -------
+    case(verbose=True, **kwargs)
+        Looks for ~/.uedgerc. Reads it if found, prints instructions
+        for creating it if not
+    create()
+        Creates ~/.uedgerc interactively from Python prompt
+
+    """
+
     def __init__(self):
+        """Creates Config object and links functions"""
         self.configured = False
         self.search = Lookup()
         self.configs = {}
-        
 
-    def case(self, verbose=True, new=True, **kwargs):
+    def case(self, verbose=True, **kwargs):
+        """Looks for and reads ~/.uedgerc
+
+        Prints instructions to creating it if file not found
+
+        Keyword arguments
+        -----------------
+        verbose : bool (default = False)
+            Switch whether to write progress to stdout (True) or not
+
+        Returns
+        -------
+        None
+        """
         from os import path
         from yaml import safe_load
         from pathlib import Path
@@ -23,8 +49,12 @@ class Config():
                 print("UEDGE configuration file {}/.uetoolsrc read.".format(searchpath))
         except:
             if verbose is True:
-                print("No UETOOLS config file found: Configure file by calling Case().config.create() or uetools.config.create()")
-                print("Alternatively, manually create the .uetoolsrc configuration YAML in your home directory.")
+                print(
+                    "No UETOOLS config file found: Configure file by calling Case().config.create() or uetools.config.create()"
+                )
+                print(
+                    "Alternatively, manually create the .uetoolsrc configuration YAML in your home directory."
+                )
             config = None
         if config is not None:
             for key, variable in config.items():
@@ -32,6 +62,7 @@ class Config():
             # NOTE: what other information to write/store?
 
     def create(self):
+        """Creates ~/.uetoolsrc based on queried input"""
         from os import path
         from yaml import dump
 
@@ -39,8 +70,10 @@ class Config():
         searchpath = path.expanduser("~")
         yes = ["yes", "y"]
         no = ["no", "n"]
-        print("Do you want to create a configuration file at "+\
-                "{}? [y/n]".format(searchpath))
+        print(
+            "Do you want to create a configuration file at "
+            + "{}? [y/n]".format(searchpath)
+        )
         try:
             # Note: This can fail in subprocess
             create = input()
@@ -58,10 +91,10 @@ class Config():
                     else:
                         paths[dirpath] = path.abspath(defpath)
                         print("    Path defined successfully!")
-            print('Do you want to define a path to a YAML variable file?')
-            print('(Used to deifne the variables to be saved by UETOOLS)')
+            print("Do you want to define a path to a YAML variable file?")
+            print("(Used to deifne the variables to be saved by UETOOLS)")
             create = input()
-            defpath='x'
+            defpath = "x"
             if (create.lower() in yes) or (len(create) == 0):
                 while not path.exists(defpath):
                     print("Define path to variableyaml:")
@@ -70,7 +103,7 @@ class Config():
                     if path.exists(defpath) is False:
                         print("Directory does not exist, please try again.")
                     else:
-                        paths['variableyamlfile'] = defpath
+                        paths["variableyamlfile"] = defpath
                         print("    Path defined successfully!")
             else:
                 print("Using standard YAML variable file.")
@@ -82,4 +115,3 @@ class Config():
         with open("{}/.uetoolsrc".format(searchpath), "w") as file:
             dump(paths, file)
         print("UEDGE config file {}.uetoolsrc successfully created!".format(searchpath))
-
