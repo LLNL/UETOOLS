d6cc71535c22816f27cdb963a448d0b35302d3c2
jasjau <jjaura@berkeley.edu>
2025-05-27 16:40:18 -0700

    Working on Distribution Area

--- src/uetools/UeVacuum/VacuumRegion.py
+++ src/uetools/UeVacuum/VacuumRegion.py
@@ -5,6 +5,8 @@ class VacuumRegion:
     def testFunction(self):
         s = Surface((2, 1), (1, 2))
         s.distribution(1.0)
+        s2 = Surface((2, 5), (4, 5))
+        s.intersectionArea(s2)
         s.fullPlot()
 
 class Surface:
@@ -100,7 +102,6 @@ class Surface:
         
         self.circle = Polygon(circlePoints) # a representation of the circle
         
-        #self.circle = self.center.buffer(radius, 5000)
         centerDist = math.sqrt((self.center.x - self.midpoint.x)**2 + (self.center.y - self.midpoint.y)**2) 
         print("Distance from center to midpoint: ", centerDist) # checks that the r_offset is correct for the specified distribution
         print("Center of circle: (", self.center.x, ", ", self.center.y, ")")
@@ -113,23 +114,40 @@ class Surface:
 
         fig, ax = subplots()
         ax.set_aspect('equal')
-        plotting.plot_line(self.segment, ax, color='red', linewidth=2)
-        plotting.plot_line(self.normal, ax, color='blue', linewidth=2)
-        plotting.plot_line(self.center, ax, color='green')
-        plotting.plot_polygon(self.circle, ax, add_points=False, color='green', linewidth=1)
+        plotting.plot_line(self.segment, ax, color='red', linewidth=2) # plots surface
+        plotting.plot_line(self.normal, ax, color='blue', linewidth=2) # plots normal line to surface
+        plotting.plot_line(self.center, ax, color='green') # plots point at center of distribution circle
+        plotting.plot_polygon(self.circle, ax, add_points=False, color='green', linewidth=1) # plots the distribution circle
+        plotting.plot_line(self.triangle, ax, color='green', linewidth=2) # plots the triangle connecting to another surface
+        plotting.plot_line(self.overlapShape, ax, color='black', linewidth=2)
         return
 
-    def intersectionArea(self, angles):
-        from shapely import Point, LineString, plotting
+    def intersectionArea(self, s2):
+        from shapely import Point, LineString, plotting, LinearRing
         import math
-        """ The parameter angles is a 1-D array of all the angles from the normal of self to the endpoints of the other 
-                surfaces. 
+        """ For now: pass in a surface object, s2, so that we can access the end points of the surface and make a triangle from the midpoint of self to the endpoints of s2.
+            The parameters s2Start and s2End would represent the start and end points of the second surface (the one we are measuring flux into from self).
+            This parameter might be changed instead to an array that represents the start and end points of all relevant surfaces to self, rather than just 2 input points.
+            """
         # find area that overlaps between circle and triangle 
         # areaOverlap / circleArea = flux on surface 2 from surface 1
-        # triangle is based on the angles we are covering from the midpoint (0 to pi)
 
-        circleArea = self.circle.area
+        # need 3 segements (LineStrings) to make the triangle
+        # s2 start to end, s2 end to s1 midpoint, s1 midpoint to s2 end
+
+        self.triangle = LinearRing([s2.start, s2.end, self.midpoint]) # triangle from midpoint of self to the endpoints of the other surface, s2
+        self.overlapShape = self.triangle.intersection(self.circle)
+        print(type(self.overlapShape))
         
+
+        overlapArea = self.overlapShape.area
+        circleArea = self.circle.area
+
+        fractionalArea = overlapArea / circleArea
+
+        print("Overlap Area: ", overlapArea)
+        print("Circle Area: ", circleArea)
+        print("Fractional Area: ", fractionalArea)
         return
 
 
