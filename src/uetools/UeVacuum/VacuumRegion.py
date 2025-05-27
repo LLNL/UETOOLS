44476825aed30a43cae2afdbad72c81f95ad4126
jasjau <jjaura@berkeley.edu>
2025-05-27 15:34:48 -0700

    Distribution Circles intersect Surface
    The distribution circles (specifically for cosine distribution) now intersect the surface.

--- src/uetools/UeVacuum/VacuumRegion.py
+++ src/uetools/UeVacuum/VacuumRegion.py
@@ -3,8 +3,8 @@ class VacuumRegion:
         return 
 
     def testFunction(self):
-        s = Surface((3, 4), (6, 5))
-        s.distribution(1)
+        s = Surface((2, 1), (1, 2))
+        s.distribution(1.0)
         s.fullPlot()
 
 class Surface:
@@ -29,8 +29,7 @@ class Surface:
             # need to make sure that we set the normal in the right direction:
                 # if end.Y > start.Y --> positive x direction
                 # if end.X < start.X --> positive y direction
-        self.normalStartX = self.midpoint.x # starting x coordinate for normal
-        self.normalStartY = self.midpoint.y # starting y coordinate for normal
+        
         self.normalEndX = self.midpoint.x # ending x coordinate for normal
         self.normalEndY = self.midpoint.y # ending y coordinate for normal
 
@@ -40,9 +39,10 @@ class Surface:
 
         self.normalHelper(dx, dy)
 
-        self.normalStart = Point(self.normalStartX, self.normalStartY) # Start point of normal
+        self.normalStart = Point(self.midpoint.x, self.midpoint.y) # Start point of normal
         self.normalEnd = Point(self.normalEndX, self.normalEndY) # End point of normal
         self.normal = LineString([self.normalStart, self.normalEnd])
+
         
         return
 
@@ -63,39 +63,51 @@ class Surface:
 
 
     def distribution(self, r_offset): # for now r_offset can only equal 0 (uniform) or 1 (cosine)
-        from shapely import Point, LineString, plotting
+        from shapely import Point, LineString, plotting, LinearRing, Polygon
         from matplotlib.pyplot import subplots
         import math
 
         radius = 1.0
-        self.center = self.normal.interpolate(r_offset)
-        self.circle = self.center.buffer(radius, 5000)
-        centerDist = math.sqrt((self.center.x - self.midpoint.x)**2 + (self.center.y - self.midpoint.y)**2) # check that the offset is correct
-        print("Distance from center to midpoint: ", centerDist)
+        self.center = self.normal.interpolate(r_offset) # the center of the circle along the normal line
+
+        numPoints = 500
+        circlePoints = []
+        # at pi, none of the x values for the circle will have been repeated, so we can add the point and then sort based on x values, descending order
+        # for the surfaces that have the normal going from the bottom to top:
+            # if endX > startX, angle is negative
+
+        if self.end.x < self.start.x: # builds circle clockwise rather than counter clockwise
+            for i in range(500, -1, -1):
+                angle = (2 * math.pi) * (i / numPoints) # calculates every angle from 0-2pi, placing 500 points to create the circle
+                x = self.center.x + radius * math.cos(angle)
+                y = self.center.y + radius * math.sin(angle)
+                if angle == math.pi:
+                    circlePoints.append((self.midpoint.x, self.midpoint.y))
+                    circlePoints = sorted(circlePoints, key=lambda x: x[0], reverse = True)
+                    i += 1
+                circlePoints.append((x, y))
+        else: 
+            for i in range(500 + 1):
+                angle = (2 * math.pi) * (i / numPoints) # calculates every angle from 0-2pi, placing 500 points to create the circle
+                x = self.center.x + radius * math.cos(angle)
+                y = self.center.y + radius * math.sin(angle)
+                if angle == math.pi:
+                    circlePoints.append((self.midpoint.x, self.midpoint.y))
+                    circlePoints = sorted(circlePoints, key=lambda x: x[0], reverse = True)
+                    i += 1
+                circlePoints.append((x, y))
+
+        
+        self.circle = Polygon(circlePoints) # a representation of the circle
+        
+        #self.circle = self.center.buffer(radius, 5000)
+        centerDist = math.sqrt((self.center.x - self.midpoint.x)**2 + (self.center.y - self.midpoint.y)**2) 
+        print("Distance from center to midpoint: ", centerDist) # checks that the r_offset is correct for the specified distribution
+        print("Center of circle: (", self.center.x, ", ", self.center.y, ")")
         return
 
-        # 1 unitize normal vector
-        # 2 multiply normal points by r_offset
-        # 3 add midpoint + multiplied points for location of center of circle
-
-        ''' #1
-        normFactor = math.sqrt((self.normalEndX-self.normalStartX)**2+(self.normalEndY-self.normalStartY)**2)
-        unitNormX = (self.normalEndX-self.normalStartX) / normFactor
-        unitNormY = (self.normalEndY-self.normalStartY) / normFactor
-        #2
-        unitNormX = unitNormX * r_offset
-        unitNormY = unitNormY * r_offset
-        #3
-        centerX = self.midpoint.x + unitNormX
-        centerY = self.midpoint.y + unitNormY
-
-        self.center = Point(centerX, centerY)
-        self.circle = self.center.buffer(radius, 5000)
-
-        centerDist = math.sqrt((centerX - self.midpoint.x)**2 + (centerY - self.midpoint.y)**2) # check that the offset is correct
-        print("Distance from center to midpoint: ", centerDist) '''
     
-    def fullPlot(self):
+    def fullPlot(self): # plots all relevant objects (surface, normal, distribution circle)
         from shapely import Point, LineString, plotting
         from matplotlib.pyplot import subplots
 
@@ -104,19 +116,20 @@ class Surface:
         plotting.plot_line(self.segment, ax, color='red', linewidth=2)
         plotting.plot_line(self.normal, ax, color='blue', linewidth=2)
         plotting.plot_line(self.center, ax, color='green')
-        plotting.plot_polygon(self.circle, ax, add_points=False, color='green', facecolor=None, linewidth=1)
+        plotting.plot_polygon(self.circle, ax, add_points=False, color='green', linewidth=1)
         return
 
-    def intersectionArea(self):
+    def intersectionArea(self, angles):
         from shapely import Point, LineString, plotting
         import math
+        """ The parameter angles is a 1-D array of all the angles from the normal of self to the endpoints of the other 
+                surfaces. 
         # find area that overlaps between circle and triangle 
-        # areaOverlap / circleArea = flux on surface
+        # areaOverlap / circleArea = flux on surface 2 from surface 1
+        # triangle is based on the angles we are covering from the midpoint (0 to pi)
 
         circleArea = self.circle.area
-        minAngle = 0
-        maxAngle = math.pi
-
+        
         return
 
 
