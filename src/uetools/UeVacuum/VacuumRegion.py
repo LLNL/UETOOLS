9b543d3048886f75e585c1c034b9a4585f2c67c9
jasjau <jjaura@berkeley.edu>
2025-05-28 11:03:47 -0700

    Cosine and Uniform Distributions
    Plots for cosine and uniform distribution shapes (for flux calculations between surfaces)

--- src/uetools/UeVacuum/VacuumRegion.py
+++ src/uetools/UeVacuum/VacuumRegion.py
@@ -3,9 +3,9 @@ class VacuumRegion:
         return 
 
     def testFunction(self):
-        s = Surface((2, 1), (1, 2))
-        s.distribution(1.0)
-        s2 = Surface((2, 5), (4, 5))
+        s = Surface((1, 4), (2, 5))
+        s.distribution(0)
+        s2 = Surface((5, 4), (5, 2))
         s.intersectionArea(s2)
         s.fullPlot()
 
@@ -15,6 +15,10 @@ class Surface:
         from shapely import Point, LineString, plotting
         from matplotlib.pyplot import subplots
         import math
+
+        """Reference Variables/Important:
+        self.start, self.end, self.midpoint, self.normalStart, self.normalEnd
+        """
 
         # Start and end should be shapely point objects?
         self.start = Point(start[0], start[1])
@@ -33,7 +37,7 @@ class Surface:
                 # if end.Y > start.Y --> positive x direction
                 # if end.X < start.X --> positive y direction
         
-        self.normalEndX = self.midpoint.x # ending x coordinate for normal
+        self.normalEndX = self.midpoint.x # ending x coordinate for normal (temporary)
         self.normalEndY = self.midpoint.y # ending y coordinate for normal
 
         # in order to get the correct slope for the normal vector
@@ -42,11 +46,10 @@ class Surface:
 
         self.normalHelper(dx, dy)
 
-        self.normalStart = Point(self.midpoint.x, self.midpoint.y) # Start point of normal
-        self.normalEnd = Point(self.normalEndX, self.normalEndY) # End point of normal
+        self.normalStart = Point(self.midpoint.x, self.midpoint.y) # Start point of normal # USE THIS FOR REFERENCE
+        self.normalEnd = Point(self.normalEndX, self.normalEndY) # End point of normal USE THIS FOR REFERENCE
         self.normal = LineString([self.normalStart, self.normalEnd])
 
-        
         return
 
     def normalHelper(self, dx, dy): # helper function to find normal direction
@@ -70,6 +73,10 @@ class Surface:
         from matplotlib.pyplot import subplots
         import math
 
+        """Reference Variables/Important:
+        self.center, self.circle
+        """
+
         radius = 1.0
         self.center = self.normal.interpolate(r_offset) # the center of the circle along the normal line
 
@@ -91,7 +98,7 @@ class Surface:
                 circlePoints.append((x, y))
         else: 
             for i in range(500 + 1):
-                angle = (2 * math.pi) * (i / numPoints) # calculates every angle from 0-2pi, placing 500 points to create the circle
+                angle = (2 * math.pi) * (i / numPoints)
                 x = self.center.x + radius * math.cos(angle)
                 y = self.center.y + radius * math.sin(angle)
                 if angle == math.pi:
@@ -104,39 +111,31 @@ class Surface:
         self.circle = Polygon(circlePoints) # a representation of the circle
         
         centerDist = math.sqrt((self.center.x - self.midpoint.x)**2 + (self.center.y - self.midpoint.y)**2) 
+
+
+        print("INFO:")
         print("Distance from center to midpoint: ", centerDist) # checks that the r_offset is correct for the specified distribution
         print("Center of circle: (", self.center.x, ", ", self.center.y, ")")
         return
 
-    
-    def fullPlot(self): # plots all relevant objects (surface, normal, distribution circle)
-        from shapely import Point, LineString, plotting
-        from matplotlib.pyplot import subplots
-
-        fig, ax = subplots()
-        ax.set_aspect('equal')
-        plotting.plot_line(self.segment, ax, color='red', linewidth=2) # plots surface
-        plotting.plot_line(self.normal, ax, color='blue', linewidth=2) # plots normal line to surface
-        plotting.plot_line(self.center, ax, color='green') # plots point at center of distribution circle
-        plotting.plot_polygon(self.circle, ax, add_points=False, color='green', linewidth=1) # plots the distribution circle
-        plotting.plot_line(self.triangle, ax, color='green', linewidth=2) # plots the triangle connecting to another surface
-        plotting.plot_line(self.overlapShape, ax, color='black', linewidth=2)
-        return
-
     def intersectionArea(self, s2):
-        from shapely import Point, LineString, plotting, LinearRing
+        from shapely import Point, LineString, plotting, Polygon
         import math
         """ For now: pass in a surface object, s2, so that we can access the end points of the surface and make a triangle from the midpoint of self to the endpoints of s2.
             The parameters s2Start and s2End would represent the start and end points of the second surface (the one we are measuring flux into from self).
             This parameter might be changed instead to an array that represents the start and end points of all relevant surfaces to self, rather than just 2 input points.
             """
+
+        """Reference Variables/Important:
+        self.triangle, self.overlapShape, overlapArea, fractionalArea
+        """
         # find area that overlaps between circle and triangle 
         # areaOverlap / circleArea = flux on surface 2 from surface 1
 
         # need 3 segements (LineStrings) to make the triangle
         # s2 start to end, s2 end to s1 midpoint, s1 midpoint to s2 end
 
-        self.triangle = LinearRing([s2.start, s2.end, self.midpoint]) # triangle from midpoint of self to the endpoints of the other surface, s2
+        self.triangle = Polygon([s2.start, s2.end, self.midpoint, s2.start]) # triangle from midpoint of self to the endpoints of the other surface, s2
         self.overlapShape = self.triangle.intersection(self.circle)
         print(type(self.overlapShape))
         
@@ -151,4 +150,30 @@ class Surface:
         print("Fractional Area: ", fractionalArea)
         return
 
-    
+    def fullPlot(self): # plots all relevant objects (surface, normal, distribution circle)
+        from shapely import Point, LineString, plotting
+        from matplotlib.pyplot import subplots, ioff
+        import matplotlib.pyplot as plt
+
+        ioff()
+        fig, ax = subplots()
+        ax.set_aspect('equal')
+        plotting.plot_line(self.segment, ax, color='red', linewidth=2) # plots surface
+        ax.text(self.start.x, self.start.y, "Surface 1", color='red')
+
+        plotting.plot_line(self.normal, ax, color='blue', linewidth=2) # plots normal line to surface
+        ax.text(self.normalEnd.x, self.normalEnd.y, "Normal (S1)", color='blue')
+
+        plotting.plot_line(self.center, ax, color='green') # plots point at center of distribution circle
+        plotting.plot_polygon(self.circle, ax, add_points=False, color='green', linewidth=1) # plots the distribution circle
+        ax.text(self.center.x, self.center.y, "Distribution Circle", color='green')
+
+        plotting.plot_polygon(self.triangle, ax, color='orange', linewidth=2) # plots the triangle connecting to another surface
+        ax.text(self.triangle.centroid.x, self.triangle.centroid.y, "Surface 2", color='orange')
+
+        plotting.plot_polygon(self.overlapShape, ax, add_points=False, color='black', linewidth=2) # displays the overlapping area
+        ax.text(self.overlapShape.centroid.x, self.overlapShape.centroid.y, "Overlap Area", color='black')
+
+        plt.show()
+
+        return
