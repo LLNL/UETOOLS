68f07e1faca1092d2e691fac1b27044eac489458
Andreas Holm <60451789+holm10@users.noreply.github.com>
2025-07-07 14:56:29 -0700

    Adds protection against duplicate limiter points

--- src/uetools/UeDEGAS/Coupling.py
+++ src/uetools/UeDEGAS/Coupling.py
@@ -744,7 +744,7 @@ class VacuumTransport_coupling:
         vessel points.
         '''
         from shapely import LinearRing, LineString
-        from numpy import array, roll
+        from numpy import array, roll, unique
         from matplotlib.pyplot import subplots
         # Get shifts and switches for upper-single null geometries
         if (self.get("rmagx") + self.get("zmagx") == 0):
@@ -805,10 +805,13 @@ class VacuumTransport_coupling:
 
         # Set start point to be first plasma mesh point
         main = roll(main_points, -(nmain_points+1), axis=0)
-        pf = roll(pf_points, -(npf_points+1), axis=0)
+        pf = roll(pf_points, -(npf_points+1), axis=0)   
         # Get index of last plasma mesh point
         nmain_vessel = len(main_points) - (nmain_points + 2)
         npf_vessel = len(pf_points) - (npf_points + 2)
+        # Remove duplicate points, assuming they only occur along the walls
+        main = unique(main, axis=0)
+        pf = unique(pf, axis=0)
 
         if plot:
             f, ax = subplots(figsize=(5,12))
